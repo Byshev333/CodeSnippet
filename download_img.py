cd94e254f2d2d882f66cb9d5e8a462999c64f0cb
baixue05 <baixue05@kuaishou.com>
2022-09-15 17:36:47 +0800

    Initial commit

---
+++ download_img.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Author ：baixue
+@Date   ：2022/9/15 16:23
+=================================================='''
+
+from argparse import ArgumentParser
+from pathlib import Path
+import urllib.request
+import multiprocessing
+from multiprocessing import Pool
+import logging
+import pandas as pd
+from PIL import Image
+import json
+import os
+import time
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36"}
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-Agent',
+                      'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+urllib.request.install_opener(opener)
+
+
+def get_chunk_data(data, chunk_size=100, ignore_last=False):
+    """对数据分块, 按照每块的数据量切分.
+    Args:
+        data: 数据, 格式需要支持索引切片.
+        chunk_size: 块大小.
+        ignore_last: 是否忽略最后一片长度不足的数据, 默认False.
+    Returns:
+        分块后的数据, 列表形式.
+    """
+    data_chunk = list()
+    for index in range(0, len(data), chunk_size):
+        data_chunk.append(data[index: index + chunk_size])
+
+    if ignore_last and data_chunk and len(data_chunk[-1]) != chunk_size:
+        return data_chunk[:-1]
+    else:
+        return data_chunk
+
+
+def __download_one(url, out_path, mylist):
+    """下载一个"""
+
+    try:
+        urllib.request.urlretrieve(url, out_path)
+        img = Image.open(out_path).convert('RGB')
+    except:
+        print(f"下载失败 {url}")
+    else:
+        mylist.append(out_path)
+
+
+def download_imgs(url_lines, item_ids, out_dir):
+    params_list = list()
+    mylist = multiprocessing.Manager().list()  # 主进程与子进程共享这个List
+    for url_suffix, id in zip(url_lines, item_ids):
+        if url_suffix:
+            complete_url = f"{url_suffix}"
+            out_path = out_dir + '/' + str(id) + '.jpg'
+            params_list.append((complete_url, out_path, mylist))
+
+    param_chunks = get_chunk_data(params_list, 100, False)
+
+    for chunk_index, param_chunk in enumerate(param_chunks):
+        with Pool(50) as pool:
+            _ = pool.starmap(__download_one, param_chunk)
+        print(f"下载完成 {(chunk_index + 1) * 100} 个图片.")
+    return mylist
+
+
+if __name__ == "__main__":
+    """命令行参数"""
+    parser = ArgumentParser(description="下载图片")
+    parser.add_argument('origin_file', type=Path, help='原始数据')
+    parser.add_argument('out_dir', type=str, help="下载图片的保存路径.")
+    args = parser.parse_args()
+    # url = 'https://ali-ad.a.yximgs.com/bs2/image-kwaishop-product/d7cc244d-cc84-44fb-aefd-07348668ab1b-i17136202.jpg'
+    # out_dir = '/ec42/baixue/dataset/plc_course/data401/item_imgs/122536978796.jpg'
+    # mylist = multiprocessing.Manager().list()  # 主进程与子进程共享这个List
+    # __download_one(url, out_dir, mylist)
+
+    print('Conda deactivate...')  # 避免Image读取错误
+    os.system('conda deactivate')
+    time.sleep(10)
+
+    print('Connect the internet...')
+    os.system('export http_proxy=http://bjrz-squid4.lf:11080 && export https_proxy=http://bjrz-squid4.lf:11080')
+    time.sleep(10)
+
+    print('Download imgs...')
+    df = pd.read_excel(args.origin_file)
+    df = df.fillna('None')
+    item_img_urls = df['item_img_url'].to_list()
+    item_ids = df['item_id'].to_list()
+    print(item_img_urls[:5])
+
+    valid_img = download_imgs(item_img_urls, item_ids, args.out_dir)
+
+    print('Need to download {} item imgs.'.format(len(item_ids)))
+    print('Download {} valid item imgs.'.format(len(valid_img)))
---
+++ preparedata2yushuang.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Author ：baixue
+@Date   ：2022/9/15 16:21
+=================================================='''
+
+import pandas as pd
+import json
+
+def preparedata2yushuang(input, output):
+    relabel_id = []
+    with open('../test_data/商业化/std_test_set/relabel.txt', 'r') as fr:
+        for line in fr:
+                    relabel_id.append(int(line.strip().split('.')[0]))
+                    print(int(line.strip().split('.')[0]))
+    df = pd.read_excel(input)
+    df = df.fillna('')
+    row_num = df.shape[0]
+    info_list = []
+    pid_list = []
+
+    file = open(output, 'a', encoding='utf-8')
+    for i in range(row_num):
+        item_id = df.loc[i, 'item_id']
+        if int(item_id) in relabel_id:
+                    item_title = df.loc[i, 'item_title']
+                    item_url = df.loc[i, 'img_url']
+                    category = df.loc[i, 'category']
+                    item_short_title = df.loc[i, 'pred_short_title']
+                    brand = df.loc[i, 'brand']
+
+                    item_info = "<img style=\"width: 200px; height: 200px;\" src=\"" + item_url + "\"/><br>" + item_title + '(' + category + ' ' + \
+                                brand + ')' + '<br>' + item_short_title
+
+                    dic_ = {"text": item_info, 'category': str(item_id)}
+                    dic_ = json.dumps(dic_, ensure_ascii=False)
+                    file.write(dic_ + '\n')
+    file.close()
+
+
+def label_for_yushuang(input, output):
+    df = pd.read_excel(input)
+    df = df.fillna('')
+    pid_list = df['photo_id'].to_list()
+    title_list = df['item_title'].to_list()
+    desc_list = df['course_desc'].to_list()
+    img_url_list = df['item_img_url'].to_list()
+    category1_list = df['first_level_category_name'].to_list()
+    category2_list = df['second_level_category_name'].to_list()
+    category3_list = df['third_level_category_name'].to_list()
+    # category4_list = df['fourth_level_category_name'].to_list()
+    valid_pid = []
+    hetu_info = []
+    item_img_info = []
+    merchandise_info = []
+
+    frameIds = []
+
+    print(len(list((set(pid_list)))))
+    print(len(df))
+    for i in range(len(df)):
+        key_list = ['剪辑', '影视', '录制', '特效', '快影']
+        if any(key in title_list[i] for key in key_list):
+                    continue
+        if pid_list[i] in set(valid_pid):
+                    continue
+        valid_pid.append(pid_list[i])
+        hetu_info.append('{}<sep>{}<sep>{}'.format(category1_list[i], category2_list[i], category3_list[i]))
+        merchandise_info.append('{}<sep>{}'.format(title_list[i], desc_list[i]))
+        item_img_info.append('<img src = "' + img_url_list[i] + '", style=" width: 200px;"/>')
+        frameIds.append("h,0,1,2,3,4,5,6,7,8")
+
+    print(len(valid_pid))
+    print(len(hetu_info))
+    print(len(merchandise_info))
+    print(len(item_img_info))
+    data = {'photoId': valid_pid,
+                'hetuInfo': hetu_info,
+                'category': merchandise_info,
+                'extData': item_img_info,
+                'frameIds': frameIds}
+
+    df_label = pd.DataFrame(data)
+    df_label.to_excel(output, index=False)
